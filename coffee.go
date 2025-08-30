f5586b2d92854a50ca1424913f343731bf95f286
Gregory <gmutafan@gmail.com>
2025-08-30 20:37:14 +0300

    update

--- coffee.go
+++ coffee.go
@@ -16,8 +16,16 @@ type Coffee struct {
 	Status      string
 }
 
-var coffeeMenu = map[string]string{
-	"coffee": "cap",
+var CoffeeDatabase = map[string]Coffee{
+	"1": {
+		ID:          1,
+		Name:        "Supremo",
+		Description: "Best coffee from Colombia",
+		Price:       120.0,
+		Weight:      250.0,
+		RoastLevel:  "Medium",
+		Status:      "empty",
+	},
 }
 
 var update string
@@ -36,7 +44,7 @@ func main() {
 
 func getAllCoffeeMenu(w http.ResponseWriter, r *http.Request) { //curl http://localhost:8080/getAllCoffeeMenu
 	w.Header().Set("Content-Type", "application/json")
-	err := json.NewEncoder(w).Encode(coffeeMenu)
+	err := json.NewEncoder(w).Encode(CoffeeDatabase)
 	if err != nil {
 		http.Error(w, "Ошибка кодирования JSON", http.StatusInternalServerError)
 		return
@@ -50,7 +58,7 @@ func addNewCoffee(w http.ResponseWriter, r *http.Request) { //-X POST -H "Conten
 	}
 	r.Body = http.MaxBytesReader(w, r.Body, 1<<20)
 
-	var newCoffies map[string]string
+	var newCoffies map[string]Coffee
 	err := json.NewDecoder(r.Body).Decode(&newCoffies)
 	if err != nil {
 		http.Error(w, "Неверный JSON: "+err.Error(), http.StatusBadRequest)
@@ -60,7 +68,7 @@ func addNewCoffee(w http.ResponseWriter, r *http.Request) { //-X POST -H "Conten
 	defer r.Body.Close() // прочесть
 
 	for key, value := range newCoffies {
-		coffeeMenu[key] = fmt.Sprintf("%v", value)
+		CoffeeDatabase[key] = value
 	}
 
 	w.Header().Set("Content-Type", "application/json")
@@ -85,13 +93,13 @@ func deleteCoffee(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	if _, ok := coffeeMenu[key]; !ok {
+	if _, ok := CoffeeDatabase[key]; !ok {
 		http.Error(w, "Такого кофе нет", http.StatusNotFound)
 		return
 	}
 
-	delete(coffeeMenu, key)
-	fmt.Printf("Удалено кофе с ключом '%s', текущее меню: %+v\n", key, coffeeMenu)
+	delete(CoffeeDatabase, key)
+	fmt.Printf("Удалено кофе с ключом '%s', текущее меню: %+v\n", key, CoffeeDatabase)
 
 	w.Header().Set("Content-Type", "application-json")
 	w.WriteHeader(http.StatusOK) //
@@ -109,7 +117,7 @@ func updateCoffeeMenu(w http.ResponseWriter, r *http.Request) {
 	}
 
 	r.Body = http.MaxBytesReader(w, r.Body, 1<<20)
-	var updateCoffee map[string]string
+	var updateCoffee map[string]Coffee
 	err := json.NewDecoder(r.Body).Decode(&updateCoffee)
 	if err != nil {
 		http.Error(w, "Неверный JSON: "+err.Error(), http.StatusBadRequest)
@@ -119,8 +127,8 @@ func updateCoffeeMenu(w http.ResponseWriter, r *http.Request) {
 	defer r.Body.Close()
 
 	for key, newValue := range updateCoffee {
-		if _, exists := coffeeMenu[key]; exists {
-			coffeeMenu[key] = newValue
+		if _, exists := CoffeeDatabase[key]; exists {
+			CoffeeDatabase[key] = newValue
 			update = key
 		} else {
 			http.Error(w, "Такой кофе не найден", http.StatusNotFound)
